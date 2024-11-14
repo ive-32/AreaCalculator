1c58a2115e00be34237a7d40e2b64c853453b34b
Ivan Savelyev <118200805+umbrellait-ivan-savelyev@users.noreply.github.com>
2024-11-14 13:04:52 +0700

    Fix readme.md

--- AreaCalculator/obj/Debug/net8.0/AreaCalculator.AssemblyInfo.cs
+++ AreaCalculator/obj/Debug/net8.0/AreaCalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AreaCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f80604c8c618e73cb9cfb5cba668f113e7b6eaf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("AreaCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AreaCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
